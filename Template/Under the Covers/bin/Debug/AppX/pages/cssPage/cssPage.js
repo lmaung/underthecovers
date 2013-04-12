e37faf82e2a6dd7a59f8e0b6ff540fbc69b06ee5
unknown <Lwin@LM-T420.(none)>
2013-04-11 19:29:09 -0500

    Blank Template

---
+++ Template/Under the Covers/bin/Debug/AppX/pages/cssPage/cssPage.js
@@ -0,0 +1,54 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+
+
+
+(function () {
+    "use strict";
+    var roamingSavedSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+
+    WinJS.UI.Pages.define("/pages/cssPage/cssPage.html", {
+
+        ready: function (element, options) {
+            var HTMLPage = document.getElementById("cmdShowHTML");
+            HTMLPage.addEventListener("click", doCmdHTMLPage, false);
+
+            var CSSPage = document.getElementById("cmdShowCSS");
+            CSSPage.addEventListener("click", doCmdCSSPage, false);
+
+            var JSPage = document.getElementById("cmdShowJS");
+            JSPage.addEventListener("click", doCmdJSPage, false);
+
+            var HomePage = document.getElementById("cmdHomePage");
+            HomePage.addEventListener("click", doCmdHomePage, false);
+        },
+
+        unload: function () {
+
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+
+        }
+    });
+    //CODE GOES HERE
+    function doCmdHTMLPage() {
+        WinJS.Navigation.navigate("/pages/htmlPage/htmlPage.html");
+    }
+
+    function doCmdCSSPage() {
+        //WinJS.Navigation.navigate("/pages/cssPage/cssPage.html");
+    }
+
+    function doCmdJSPage() {
+        WinJS.Navigation.navigate("/pages/jsPage/jsPage.html");
+    }
+
+    function doCmdHomePage() {
+        WinJS.Navigation.navigate("/pages/demoPage/demoPage.html");
+    }
+
+})();
